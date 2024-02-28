4ebde4be182b1bd609e68074db3bac94f56d3efe
Adam <metas.wang@gmail.com>
2024-02-28 19:40:12 +0800

    1. code clean 2.fix: param type issue in video clip render callback function

--- bao/utils/chat_template.py
+++ bao/utils/chat_template.py
@@ -5,6 +5,9 @@ from langchain_core.documents import Document
 
 from bao.utils.strings import seconds_to_hh_mm_ss
 
+RENDER_YOUTUBE_CLIP_FN = "fn_render_video_clip"
+SHOW_ALL_QUOTES = "show_all_quotes"
+
 
 def gen_refence(
     documents: List[Document],
@@ -24,9 +27,7 @@ def gen_refence(
         ref = documents[i].metadata.get(meta_video_key)
         if ref not in seen_refs:
             ref_docs.append(i)
-        seen_refs[ref].add(
-            seconds_to_hh_mm_ss(documents[i].metadata.get(meta_start_at_key, 0))
-        )
+        seen_refs[ref].add(documents[i].metadata.get(meta_start_at_key, 0))
 
     references_section = ""
 
@@ -52,9 +53,18 @@ def gen_refence(
                 quote_text += "\n> " + extract_quote + f"{find_ref_no(d)}\n\n"
 
     references_section += quote_text + "\n"
+    render_fn = (
+        other_keys.get(RENDER_YOUTUBE_CLIP_FN)
+        if RENDER_YOUTUBE_CLIP_FN in other_keys
+        and callable(other_keys[RENDER_YOUTUBE_CLIP_FN])
+        else lambda x: seconds_to_hh_mm_ss(x[-1])
+    )
     for i in ref_docs:
         document = documents[i]
-        start_at = ", ".join(sorted(seen_refs[document.metadata.get(meta_video_key)]))
-        reference_line = f"{i+1}. [{document.metadata.get(meta_title_key)}]({document.metadata.get(meta_source_key)}) {start_at}\n"
+        # render_fn
+        start_seconds = sorted(seen_refs[document.metadata.get(meta_video_key)])
+        clip_links = [render_fn(document.metadata.get(meta_video_key), s) for s in start_seconds]  # type: ignore
+        video_clips = ", ".join(clip_links)
+        reference_line = f"{i+1}. [{document.metadata.get(meta_title_key)}]({document.metadata.get(meta_source_key)}) {video_clips}\n"
         references_section += reference_line
     return references_section
